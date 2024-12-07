de5702a3a8383a6821896a062749bc0085a26ee4
dharaneesh-cmd <dharaneesh127@gmail.com>
2024-12-07 23:56:36 +0530

    Created R53 class only added 5 methods rest 53 - 5 will be commited soon!

---
+++ SolutionJava/src/Random53ProblemAndSolution.java
@@ -0,0 +1,50 @@
+public class Random53ProblemAndSolution {
+    public static void main(String[] args) {
+//        printNumbers();
+//        printOddNumbers();
+//        multipleOf(5, 10);
+//        multipleOfRange(1, 9, 3);
+        calculateSum(1, 5);
+    }
+
+//  #1  Print Numbers from 1 to 10
+    public static void printNumbers() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Printing " + i);
+        }
+    }
+
+//  #2  Print Odd Numbers Less Than 100
+    public static void printOddNumbers() {
+        for (int i = 1; i < 100; i++ ){
+            if (i % 2 != 0)
+                System.out.println("Odd number " + i);
+        }
+    }
+
+//  #3  Print Multiplication Table with given number and length
+    public static void multipleOf(int number, int length) {
+        for (int i = 1; i <= length ; i++) {
+            System.out.println(number + " * " + i + " = " + number * i);
+        }
+    }
+
+//  #4  Print All Multiplication Tables with given range and length
+    public static void multipleOfRange(int start, int end, int length) {
+        for (int i = start; i <= end; i++) {
+            for (int j = 1; j <= length; j++) {
+                System.out.println(i + " * " + j + " = " + i * j);
+            }
+        }
+    }
+
+//  #5  Calculate Sum of Numbers from 1 to 10
+    public static void calculateSum(int a, int b) {
+        int x = 0;
+        for (int i = a; i <= b ; i++) {
+            x = x + i;
+        }
+        System.out.println(x);
+    }
+
+}
